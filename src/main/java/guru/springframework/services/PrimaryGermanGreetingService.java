b0358b211366a1c964aacfde4791bef55bc35401
sergejhorvat <sergejhorvat@users.noreply.github.com>
2019-03-08 20:56:55 +0100

    Fixed issues with null pointer for different languages.

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,5 +1,7 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -8,13 +10,15 @@ import org.springframework.stereotype.Service;
  * Created by jt on 5/24/17.
  */
 @Service
-@Primary
 @Profile("de")
+@Primary
 public class PrimaryGermanGreetingService implements GreetingService {
 
+    @Autowired
     private GreetingRepository greetingRepository;
 
     @Override
+    @Qualifier("greetingServiceImpl")
     public String sayGreeting() {
         return greetingRepository.getGermanGreeting();
     }
